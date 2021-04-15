14c59c2f53f3c20f75fbd3ad302036def84259b2
xxKeefer <xxkeefer.code@gmail.com>
2021-04-15 19:10:43 +1000

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+function App(): ReactNode {
+  return (
+    <div className="App">
+      <div>init commit</div>
+    </div>
+  );
+}
+
+export default App;
